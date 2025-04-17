899da40b581dc53263f4ef5c8c18425460af6745
Haitham <ale3x90@gmail.com>
2025-04-17 23:48:12 +0200

    change name to make it more proper here..

--- utility/buildravdess.py
+++ utility/buildravdess.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 
 
-def buildravdess(dataset_path):
+def buildravdess(ravdess_path):
     emotion_map = {
         1: 'neutral',
         2: 'calm',
@@ -16,8 +16,8 @@ def buildravdess(dataset_path):
 
     data = []
 
-    for actor_folder in sorted(os.listdir(dataset_path)): # actor level loop
-        actor_path = os.path.join(dataset_path, actor_folder)
+    for actor_folder in sorted(os.listdir(ravdess_path)): # actor level loop
+        actor_path = os.path.join(ravdess_path, actor_folder)
         actor_id = int(actor_folder.split('_')[1])  # split into a list of something like ["Actor", "01"], it removes the _ for each actor, and we take index 1
 
         for filename in sorted(os.listdir(actor_path)): # in-actor level loop
@@ -25,7 +25,7 @@ def buildravdess(dataset_path):
                 parts = filename.split('-')
                 emotion_code = int(parts[2]) # since emotion is the third number in the filename and it splits the filename into a list of all the information
                 emotion = emotion_map[emotion_code]
-                full_path = os.path.join(dataset_path, actor_folder, filename)
+                full_path = os.path.join(ravdess_path, actor_folder, filename)
 
                 data.append([full_path, emotion, actor_id])
 
